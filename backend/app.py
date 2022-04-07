47060a293e7b48a52b2d80f90d60c288e128d9be
Will R <wregann@bu.edu>
2022-04-07 16:47:21 -0400

    Most backend functions now return json properly

--- backend/app.py
+++ backend/app.py
@@ -86,13 +86,15 @@ def get_weather_data(city_string):
         lat, lon = wa.get_lat_lon_from_input(key=creds.OPENWEATHER_KEY, city=city_string)
     except ValueError as e:
         print(e)
-        return 'Error: Could not get city longitude and latitude, please try agian'
+        print('Error: Could not get city longitude and latitude, please try again')
+        return redirect("/")
     
     try:
         five_day_data_dict = wa.get_5_day_data(key=creds.OPENWEATHER_KEY, lat=lat, lon=lon)
     except ValueError as e:
         print(e)
-        return 'Error: could not get weather data from latitude and lognitude'
+        print('Error: could not get weather data from latitude and lognitude')
+        return redirect("/")
     print("----------------------------END OF GET WEATHER DATA--------------------------------")
     return five_day_data_dict
 
@@ -116,14 +118,14 @@ def verify():
 # Spotify returns requested data
 @app.route("/go", methods=['POST', 'GET'])
 def go():
+    response_dict = {"Status": "Failure", "Name" : "", "Error" : ""}
     print("----------------------------GO--------------------------------")
     print("Current uuid: ", session.get('uuid'))
     cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
     sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id = creds.SPOTIFY_ID, client_secret = creds.SPOTIFY_SECRET, redirect_uri = REDIRECT_URI, scope = SCOPE, cache_handler=cache_handler)
     if not sp_oauth.validate_token(cache_handler.get_cached_token()):
-        print("Error in getting valid token, sent back")
-        print("----------------------------END OF GO--------------------------------")
-        return redirect('/')
+        response_dict['Error'] = "Error in getting valid token, sent back"
+        return response_dict
 
     sp = spotipy.Spotify(auth_manager=sp_oauth)
     spotify_user_id = sp.me()['id']
@@ -139,11 +141,7 @@ def go():
     cursor.execute("SELECT user_id FROM user_dates WHERE user_id = %s LIMIT 1;", [spotify_user_id])
     if len(cursor.fetchall()) > 0:
         in_system = True
-    try:
-        #Closing the cursor
-        cursor.close()
-    except:
-        print("Error in getting Spotify Username")
+    cursor.close()
     
     # If user is in system, songs are already in database
     # If user is not in system
@@ -159,19 +157,30 @@ def go():
                 break
             cur_tracks = [x['track'] for x in response['items']]
             new_ids = [x['id'] for x in cur_tracks]
+               
             tracks.extend(new_ids)
             feats = sp.audio_features(new_ids)
+            
+            none_indicies = [i for i in range(len(feats)) if feats[i] == None]
+            if len(none_indicies) > 0:
+                print("At least one song present that does not have spotify features")
+                response_dict["Error"] = "Error in getting at least one song's spotify features"
+                # Not returning response_dict because program could still mostly succeed
+            cur_tracks = [t for i, t in enumerate(cur_tracks) if i not in none_indicies]
+            feats = [f for i, f in enumerate(feats) if i not in none_indicies]
+            
+            
             songs_to_add.extend([tuple([cur_tracks[x]['id'], cur_tracks[x]['name'], cur_tracks[x]['album']['artists'][0]['name'], 
                                 feats[x]['energy'], feats[x]['tempo'], feats[x]['danceability'], feats[x]['loudness'],
-                                feats[x]['valence'], feats[x]['speechiness'], feats[x]['instrumentalness']]) for x in range(len(cur_tracks))])
+                                feats[x]['valence'], feats[x]['instrumentalness']]) for x in range(len(cur_tracks))])
             if len(response['items']) < 50:
                 break
         
         #Creating a connection cursor
         cursor = mysql.connection.cursor()
         sql = """INSERT IGNORE INTO song_stats 
-        (song_id,name,artist,energy,tempo,danceability,loudness,valence,speechiness,instrumentalness)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+        (song_id,name,artist,energy,tempo,danceability,loudness,valence,instrumentalness)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"""
         cursor.executemany(sql, songs_to_add)
         sql2 = "INSERT IGNORE INTO user_songs (song_id,user_id) VALUES (%s, %s);"
         cursor.executemany(sql2, [[x, spotify_user_id] for x in tracks])
@@ -183,6 +192,8 @@ def go():
             cursor.close()
         except:
             print("Error in adding songs to database")
+            response_dict["Error"] = "Error in adding liked songs to database"
+            return response_dict
         
     # Update User Access Time
     cursor = mysql.connection.cursor()
@@ -193,14 +204,15 @@ def go():
     # Get user liked songs population statistics
     get_stats_sql = """SELECT AVG(tempo) AS tempo_avg, STD(tempo) AS tempo_std, 
         AVG(energy) as energy_avg, STD(energy) as energy_std, 
-        AVG(valence) AS AVG_VALENCE, STD(valence) AS STD_VALENCE 
+        AVG(valence) AS valence_avg, STD(valence) AS valence_std
         FROM song_stats 
         WHERE song_id IN (SELECT song_id FROM user_songs WHERE user_id = %s);"""
     cursor.execute(get_stats_sql, [spotify_user_id])
     row = cursor.fetchone()
-    print("Collected Stats: ", row)
     if not (row == None or len(row) == 0 or row[0] == None):
-        user_pop_stats = [round(float(x),3) for x in row]
+        row_headers=[x[0] for x in cursor.description]
+        user_pop_stats = dict(zip(row_headers, [round(float(x),3) for x in row]))
+        print("Collected Stats: ", user_pop_stats)
     else:
         print("Error in getting songs of user, returned no songs")
         return redirect("/")
@@ -212,13 +224,7 @@ def go():
                                                             user_pop_stats=user_pop_stats) + ";"
     cursor.execute(custom_sql, [spotify_user_id])
     custom_ids = [x[0] for x in cursor.fetchall()]
-    try:
-        #Saving the Actions performed on the DB
-        mysql.connection.commit()
-        #Closing the cursor
-        cursor.close()
-    except:
-        print("Error in getting song custom id selection from database")
+    cursor.close()
     
     playlist_name = "Weatherify Test Playlist (Temp={0}) (Weather={1})".format(temperature, weather_description)
     playlists = [p['name'] for p in sp.user_playlists(user=spotify_user_id)['items']]
@@ -233,8 +239,10 @@ def go():
                 sp.user_playlist_add_tracks(user=spotify_user_id, playlist_id=playlist[1], tracks=custom_ids[off:off+100])
             break
     print("Playlist Created: ", playlist_name, " for ", spotify_user_id)
-    print("----------------------------END OF GO--------------------------------")
-    return redirect("/")
+    print("----------------------------END OF GO---------------------------------------------")
+    response_dict["Status"] = "Success"
+    response_dict["Name"] = playlist_name
+    return response_dict
 
 # Get user information out of table
 @app.route("/out", methods=['GET'])
@@ -278,15 +286,9 @@ def remove_info():
 
 # Generates the second half of the SQL statement for song parameters based on weather description
 def get_custom_playlist_sql(main_weather: str, description_weather: str, temp: int,
-                            user_pop_stats: list[float]) -> str:
+                            user_pop_stats: dict) -> str:
     """
-    list user_pop_stats :
-       [0] : tempo average
-       [1] : tempo std
-       [2] : energy average
-       [3] : energy std
-       [4] : valence average
-       [5] : valence std
+    dict user_pop_stats : tempo_avg, tempo_std, energy_avg, energy_std, valence_avg, valence_std
     """
     tempo_lower = 0
     tempo_upper = 9999
@@ -321,9 +323,11 @@ def current_user():
         print("failed to get current user")
         return redirect('/')
     spotify = spotipy.Spotify(auth_manager=auth_manager)
-    print("Current User: ", spotify.me()['id'])
+    cur_id = spotify.me()['id']
+    print("Current User: ", cur_id)
     print("----------------------END OF CURRENT USER--------------------------------")
-    return redirect("/")
+    response_dict = {"User" : cur_id}
+    return response_dict
 
 if __name__ == "__main__":
     app.run(port=1000, debug=True)
